2c8394b1c0fe987b7b25f3b4850e45e539952586
Danny Clyde <Dantheman27c@gmail.com>
2020-02-07 20:46:29 -0700

    skeleton

--- src/Assign3.java
+++ src/Assign3.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Assign3 {
+	static Scanner input = new Scanner(System.in);
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		String cmd = "";
+		while(!cmd.equals("exit")) {
+			System.out.println(cmd);
+			cmd = getNextCommand();
+		}
+	}
+
+	public static String getNextCommand() {
+		System.out.print("[" + System.getProperty("user.dir") + "]: ");
+		return input.nextLine();
 	}
 }
